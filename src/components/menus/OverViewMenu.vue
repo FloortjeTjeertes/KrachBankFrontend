8d6ffab1df599145b8ac1131a2e71d5aba425ecd
Floortje <florian.tjeertes@ziggo.nl>
2025-05-27 23:00:44 +0200

    feat: Add Overview menu item to account overview navigation

--- src/components/menus/OverViewMenu.vue
+++ src/components/menus/OverViewMenu.vue
@@ -9,6 +9,7 @@ const emit  = defineEmits(['change']);
             <h4>Account Overview</h4>
         </header>
         <ul>
+            <li><p @click="emit('change', 'overview')">Overview</p></li>
             <li><p @click="emit('change', 'transactions')">Transactions</p></li>
             <li><p @click="emit('change', 'find-iban')">Find IBAN</p></li>
         </ul>
