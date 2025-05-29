e1c4cb939f5090257d8d47b522de4c9e511f5e2e
Jochem Schippers <52064556+jochemschippers@users.noreply.github.com>
2025-05-29 09:55:19 +0200

    small fixes

--- src/components/common/SignUpForm.vue
+++ src/components/common/SignUpForm.vue
@@ -3,22 +3,58 @@
     <h2>Sign up</h2>
     <form @submit.prevent="submitSignup">
       <label for="firstName">First Name</label>
-      <input type="text" id="firstName" v-model="firstName" placeholder="Enter your first name" required />
+      <input
+        type="text"
+        id="firstName"
+        v-model="form.firstName"
+        placeholder="Enter your first name"
+        required
+      />
 
       <label for="lastName">Last Name</label>
-      <input type="text" id="lastName" v-model="lastName" placeholder="Enter your last name" required />
+      <input
+        type="text"
+        id="lastName"
+        v-model="form.lastName"
+        placeholder="Enter your last name"
+        required
+      />
 
       <label for="email">Email</label>
-      <input type="email" id="email" v-model="email" placeholder="Enter your email" required />
+      <input
+        type="email"
+        id="email"
+        v-model="form.email"
+        placeholder="Enter your email"
+        required
+      />
 
       <label for="bsn">BSN</label>
-      <input type="text" id="bsn" v-model="bsn" placeholder="Enter your BSN" required />
+      <input
+        type="text"
+        id="bsn"
+        v-model="form.bsn"
+        placeholder="Enter your BSN"
+        required
+      />
 
       <label for="password">Password</label>
-      <input type="password" id="password" v-model="password" placeholder="Enter your password" required />
+      <input
+        type="password"
+        id="password"
+        v-model="form.password"
+        placeholder="Enter your password"
+        required
+      />
 
       <label for="phoneNumber">Phone Number</label>
-      <input type="tel" id="phoneNumber" v-model="phoneNumber" placeholder="Enter your phone number" required />
+      <input
+        type="tel"
+        id="phoneNumber"
+        v-model="form.phoneNumber"
+        placeholder="Enter your phone number"
+        required
+      />
 
       <button type="submit">Sign up</button>
     </form>
@@ -27,44 +63,44 @@
       Already have an account?
       <a href="#" @click.prevent="$emit('switch-to-login')">Log in</a>
     </p>
+
+    <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+    <p v-if="successMessage" class="success">{{ successMessage }}</p>
   </div>
 </template>
 
 <script>
-import { createUser } from '@/queries/users';
+import { createUser } from "@/queries/users";
 
 export default {
-  name: 'SignUpForm',
-  emits: ['signup-submitted', 'switch-to-login'],
+  name: "SignUpForm",
+  emits: ["signup-submitted", "switch-to-login"],
   data() {
     return {
-      firstName: '',
-      lastName: '',
-      email: '',
-      bsn: '',
-      password: '',
-      phoneNumber: '',
+      form: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        bsn: "",
+        password: "",
+        phoneNumber: "",
+      },
+      errorMessage: "",
+      successMessage: "",
     };
   },
   methods: {
     async submitSignup() {
-      const userData = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        email: this.email,
-        bsn: this.bsn,
-        password: this.password,
-        phoneNumber: this.phoneNumber,
-      };
-
+      this.errorMessage = "";
+      this.successMessage = "";
       try {
-        const createdUser = await createUser(userData);
-        console.log('Signup successful:', createdUser);
-        this.$emit('signup-submitted', createdUser);
+        const response = await createUser(this.form);
+        this.successMessage = "Signup successful!";
+        this.$emit("signup-submitted", response);
       } catch (error) {
-        const message =
-          error.response?.data?.message || error.message || 'Signup failed';
-        alert(message);
+        console.error("Signup failed:", error);
+        this.errorMessage =
+          error.response?.data?.message || "Signup failed. Please try again.";
       }
     },
   },
@@ -81,12 +117,26 @@ h2 {
   text-align: center;
   font-size: 0.9em;
 }
+
 .login-link a {
   color: var(--pico-primary);
   text-decoration: none;
   cursor: pointer;
 }
+
 .login-link a:hover {
   text-decoration: underline;
+}
+
+.error {
+  color: red;
+  margin-top: 1rem;
+  text-align: center;
+}
+
+.success {
+  color: green;
+  margin-top: 1rem;
+  text-align: center;
 }
 </style>
